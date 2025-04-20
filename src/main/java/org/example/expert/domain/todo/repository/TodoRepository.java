1b98eb79e5773a0287a0277d6ef2dd42fece8828
류형철 <fbgudcjf2@gmail.com>
2025-04-21 01:45:04 +0900

    레벨 2-1: N+1 문제

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -3,6 +3,7 @@ package org.example.expert.domain.todo.repository;
 import org.example.expert.domain.todo.entity.Todo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,13 +12,17 @@ import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
-    @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
+    @EntityGraph(attributePaths = {"user"})
     Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
 
-    @Query("SELECT t FROM Todo t " +
-            "LEFT JOIN FETCH t.user " +
-            "WHERE t.id = :todoId")
-    Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
+    /*N+1 : 쿼리가 해당객체가 가지고있는 연관관계 또한 조회하게 됨.
+    * JOIN FETCH 를 통해 N+1문제 해결한상태
+    * 연관된 엔티티를 즉시 로딩*/
+
+//            "LEFT JOIN FETCH t.user " +
+//            "WHERE t.id = :todoId")
+    @EntityGraph(attributePaths ={"user"})
+    Optional<Todo> findByIdWithUser(Long todoId);
 
     int countById(Long todoId);
 }
